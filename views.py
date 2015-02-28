e6b4e4bde63d5bf355b5706ba9eebfa5a0b4fa45
Raj Shah <brainix@gmail.com>
2015-02-28 02:12:00 -0800

    pivoted Little Wiki into Autonome

--- views.py
+++ views.py
@@ -8,8 +8,7 @@
 
 
 import os
-
-import wikipedia
+import random
 
 from flask import request
 from twilio.rest import TwilioRestClient
@@ -21,13 +20,24 @@ from app import app
 @app.route('/sms', methods=['POST'])
 def sms():
     to = request.form.get('From')
-    title = request.form.get('Body')
-    wikipedia.set_lang('simple')
-    summary = wikipedia.summary(title, sentences=2, chars=140)
+    command = request.form.get('Body')
+
+    if command == 'STATUS':
+        bodies = [
+            'Your car is NEXT IN LINE and will be ready for pick up in 2 HOURS.',
+            'Your car is GETTING ITS OIL CHANGED and will be ready for pick up in 1 HOUR AND 30 MINUTES.',
+            'Your car is GETTING ITS FILTER CHANGED and will be ready for pick up in 1 HOUR.',
+            'Your car is GOING THROUGH A 128 POINT INSPECTION and will be ready for pick up in 30 MINUTES.',
+            'Your car is BEING DETAILED and will be ready for pick up in 15 MINUTES.',
+            'Your car is READY FOR PICK UP NOW.'
+        ]
+        body = random.choice(body)
+    else:
+        body = 'Text STATUS to get the status of your car, UNSUBSCRIBE to receive no further SMSes, or SUBSCRUBE to re-subscribe to future SMSes.'
 
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
     from_ = os.environ['TWILIO_NUMBER']
     client = TwilioRestClient(account_sid, auth_token)
-    client.messages.create(to=to, from_=from_, body=summary)
+    client.messages.create(to=to, from_=from_, body=body)
     return ''
